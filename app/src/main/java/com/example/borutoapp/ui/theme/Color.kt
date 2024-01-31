90302ef86b5d2699cd1cfc47033f150e168e2269
Harsh Vardhan <devharsh2k4@gmail.com>
2024-02-01 02:42:43 +0530

    onboarding page added

--- app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
@@ -1,6 +1,11 @@
 package com.example.borutoapp.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.White
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -14,3 +19,31 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val LightGray = Color(0XFFD8D8D8)
+val DarkGray = Color(0XFF2A2A2A)
+
+val Colors.welcomeScreenBackgroundColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) Black else White
+
+val Colors.titleColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) LightGray else DarkGray
+
+val Colors.descriptionColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) LightGray.copy(alpha = 0.5f)
+    else DarkGray.copy(alpha = 0.5f)
+
+val Colors.activeIndicatorColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) Purple700 else Purple500
+
+val Colors.inactiveIndicatorColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) DarkGray else LightGray
+
+val Colors.buttonBackgroundColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) Purple700 else Purple500
